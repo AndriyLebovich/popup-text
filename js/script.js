e24ba34dc75128549b2829f2558b7d8d37b518d8
Andriy_Lebovich <lebovichandriy@gmail.com>
2024-06-22 00:43:35 +0200

    popap.projekt

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
 
 
-const toggleBtn = document.querySelector('.toggle__btn');
-const hiddenContent = document.querySelector('.hidden__content');
 
-if (toggleBtn) {
 
-	toggleBtn.addEventListener("click", function (e) {
-		hiddenContent.classList.toggle('._show__content');
-	});
+function readMore() {
+
+	var btnText = document.getElementById("btn__text");
+	var hiddenContent = document.getElementById("hidden__content");
+
+
+	hiddenContent.classList.toggle('_show');
+
+	if (btnText.innerHTML === 'click here') {
+		btnText.innerHTML = 'thanks!';
+	} else {
+		btnText.innerHTML = 'click here';
+	}
 
 }
 
+
+
+
+
+/*
+if (toggleBtn) {
+
+	tB.addEventListener( function readMore () {
+		hC.classList.toggle('show__content');
+	});
+
+}
+*/
 
 
 
